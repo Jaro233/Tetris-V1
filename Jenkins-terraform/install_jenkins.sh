c45e7d01846f63e5ffb0d96c14350e204493cc31
Jaro233 <jaroslaw.waliczek@vp.pl>
2023-11-07 12:47:40 +0100

    changed terraform scripts and added Jenkinsfile

--- Jenkins-terraform/install_jenkins.sh
+++ Jenkins-terraform/install_jenkins.sh
@@ -12,7 +12,7 @@ sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 
-#install docker
+#install docker and sonarqube
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu  
